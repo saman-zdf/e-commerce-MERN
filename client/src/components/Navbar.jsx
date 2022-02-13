35a7179d04fb37894f09bba0121d8ded0620b0de
saman-zdf <samanfyz68@gmail.com>
2022-02-14 06:48:21 +0800

    navbar has been added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import {
   Container,
   Wrapper,
@@ -8,18 +6,36 @@ import {
   Right,
   Language,
   SearchContainer,
+  Input,
+  Logo,
+  MenuItem,
 } from '../style/Navbar';
-
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import Badge from '@mui/material/Badge';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 const Navbar = () => {
   return (
     <Container>
       <Wrapper>
         <Left>
           <Language>EN</Language>
-          <SearchContainer></SearchContainer>
+          <SearchContainer>
+            <Input />
+            <SearchOutlinedIcon style={{ color: 'grey', fontSize: 16 }} />
+          </SearchContainer>
         </Left>
-        <Center>Center</Center>
-        <Right>Right</Right>
+        <Center>
+          <Logo>The Iconic</Logo>
+        </Center>
+        <Right>
+          <MenuItem>Register</MenuItem>
+          <MenuItem>Sign In</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color='primary'>
+              <ShoppingCartOutlinedIcon />
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
