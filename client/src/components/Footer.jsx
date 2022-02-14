b0ac9e6199f51503f35eab1cd9fb55c899a9776e
saman-zdf <samanfyz68@gmail.com>
2022-02-14 15:41:52 +0800

    footer has been added, home page is done

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,11 +7,19 @@ import {
   Desc,
   SocialContainer,
   SocialIcon,
+  Title,
+  List,
+  ListItem,
+  ContactItem,
+  Payment,
 } from '../style/Footer';
 import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import PinterestIcon from '@mui/icons-material/Pinterest';
+import FmdGoodIcon from '@mui/icons-material/FmdGood';
+import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
+import EmailIcon from '@mui/icons-material/Email';
 const Footer = () => {
   return (
     <Container>
@@ -39,8 +47,36 @@ const Footer = () => {
           </SocialIcon>
         </SocialContainer>
       </Left>
-      <Center>Center</Center>
-      <Right>Right</Right>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <FmdGoodIcon style={{ marginRight: '10px' }} />
+          Mitchel Ave, Yongah Hill, Perth, Wa, Australia
+        </ContactItem>
+        <ContactItem>
+          <PhoneAndroidIcon style={{ marginRight: '10px' }} />
+          +61431157370
+        </ContactItem>
+        <ContactItem>
+          <EmailIcon style={{ marginRight: '10px' }} />
+          samanfyz68@gmail.com
+        </ContactItem>
+        {/* <Payment src='https://i.ibb.co/QFvn4z6/payment.png' /> */}
+      </Right>
     </Container>
   );
 };
--- client/src/style/Footer.js
+++ client/src/style/Footer.js
@@ -15,6 +15,7 @@ export const Logo = styled.h2``;
 
 export const Desc = styled.p`
   margin: 20px 0px;
+  max-width: 350px;
 `;
 
 export const SocialContainer = styled.div`
@@ -35,8 +36,36 @@ export const SocialIcon = styled.div`
 
 export const Center = styled.div`
   flex: 1;
+  padding: 20px;
+`;
+export const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+export const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
 `;
 
 export const Right = styled.div`
   flex: 1;
+  padding: 20px;
+`;
+
+export const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+export const Payment = styled.img`
+  width: 100%;
 `;
